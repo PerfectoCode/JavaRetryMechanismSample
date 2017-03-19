cbbfa51645394bd730e2c403a37154582f2239da
Liron Avraham <lirona@nb-lirona-macosx.local>
2017-03-19 11:53:00 +0200

    Added failed scenario handle

--- src/test/java/RemoteWebDriverAspect.java
+++ src/test/java/RemoteWebDriverAspect.java
@@ -13,30 +13,41 @@ public class RemoteWebDriverAspect {
         System.out.println("Before Creating driver...");
     }
 
-
     @Around("call(org.openqa.selenium.remote.RemoteWebDriver+.new(..))")
     @SuppressWarnings("PMD.AvoidCatchingThrowable")
     public Object remoteWebDriverAspect(ProceedingJoinPoint point) throws Throwable {
-        System.out.println("\nTrying to create the driver current time: " + System.currentTimeMillis());
-        Object proceed = null;
+
+        //Code to run before creating the driver
+        long start = System.currentTimeMillis() / 1000;
+        System.out.println("\n[" + System.currentTimeMillis() + "] Trying to create a Remote Web Driver");
+        Object driver = null;
         int numOfRetries = 0;
-        while (proceed ==null && numOfRetries < Constants.MaxTimesToRetry) {
+        while (driver == null & numOfRetries < Constants.MaxTimesToRetry) {
             try {
-                System.out.println("Retry : " + numOfRetries);
-                proceed = point.proceed();
+                System.out.println("[" + System.currentTimeMillis() + "] Try number : " + numOfRetries);
+                driver = point.proceed();
             } catch (Throwable throwable) {
-                System.out.println("Failed to allocate device ...");
+                System.out.println("[" + System.currentTimeMillis() + "] Device allocation failed");
                 String message = throwable.getMessage();
                 System.out.println(message);
                 numOfRetries++;
                 Thread.sleep(Constants.WaitOnRetry);
             }
         }
-        System.out.println("Got a driver current time: "+ System.currentTimeMillis());
-        return proceed;
+
+        if (driver != null) {
+            //Code to run after successfully creating a driver
+            System.out.println("[" + System.currentTimeMillis() / 1000 + "] Remote Web Driver initialized successfully");
+        }
+
+        else {
+            //Code to run when used up retries with no success
+            System.out.println("[" + System.currentTimeMillis() + "] Failed to initialize a Remote Web Driver");
+            //Throw exception?
+        }
+
+        return driver;
     }
-
-
 
 
 }
